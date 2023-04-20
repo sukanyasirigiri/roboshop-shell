e7f4398cce1d379a628ba7299187412ecca7a7f2
sukanya <sukanyasirigiri@gmail.com>
2023-04-20 15:02:41 -0400

    restart catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -27,7 +27,7 @@ cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.s
 echo -e "\e[36m>>>>>>>> start catalogue service <<<<<<<<<\e[0m"
 systemctl daemon-reload
 systemctl enable catalogue 
-systemctl start catalogue
+systemctl restart catalogue
 
 echo -e "\e[36m>>>>>>>> copy mongodb repo <<<<<<<<<\e[0m"
 cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
