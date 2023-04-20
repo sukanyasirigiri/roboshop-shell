a3da02835f2d53f1b6baf50de3b403b75e0621fb
sukanya <sukanyasirigiri@gmail.com>
2023-04-20 14:14:51 -0400

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -22,7 +22,7 @@ echo -e "\e[36m>>>>>>>> install nodejs dependencies <<<<<<<<\e[0m"
 npm install 
 
 echo -e "\e[36m>>>>>>>> copy catalogue systemD file <<<<<<<<\e[0m"
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service
 
 echo -e "\e[36m>>>>>>>> start catalogue service <<<<<<<<\e[0m"
 systemctl daemon-reload
