57ba1ce4972a5939d5a95b390a37864615061caf
sukanya <sukanyasirigiri@gmail.com>
2023-04-21 15:40:38 -0400

    shipping service

--- shipping.sh
+++ shipping.sh
@@ -27,7 +27,7 @@ echo -e "\e[36m>>>>>>>> load schema <<<<<<<<<\e[0m"
 mysql -h mysql.devops1722.com -uroot -pRoboShop@1 < /app/schema/shipping.sql 
 
 echo -e "\e[36m>>>>>>>> setup systemdservice <<<<<<<<<\e[0m"
-cp /home/centos/Roboshop.sh/shipping.service /etc/systemd/system/shipping.service
+cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service
 
 echo -e "\e[36m>>>>>>>> start shipping service <<<<<<<<<\e[0m"
 systemctl daemon-reload
