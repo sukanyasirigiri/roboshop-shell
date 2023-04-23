04a51a2d4e1674cbb6e07da772bc766ea01e8944
sukanya <sukanyasirigiri@gmail.com>
2023-04-23 11:59:21 -0400

    script path

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,6 +1,6 @@
 
-
-script_path=$(dirname $0)
+script=$(realpath "0")
+script_path=$(dirname "$script")
 source ${script_path}/common.sh
 
 echo -e "\e[36m>>>>>>>> setup Erlang repos <<<<<<<<<\e[0m"
--- redis.sh
+++ redis.sh
@@ -1,4 +1,8 @@
 
+script=$(realpath "0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
+
 echo -e "\e[36m>>>>>>>> install redis repos <<<<<<<<<\e[0m"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
 
