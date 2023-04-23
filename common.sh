33e824ce79c6680623180e33eff13676ae6ec792
sukanya <sukanyasirigiri@gmail.com>
2023-04-23 19:30:06 -0400

    common

--- common.sh
+++ common.sh
@@ -2,40 +2,48 @@ app_user=roboshop
 script_path=$(dirname $0)
 source ${script_path}/common.sh
 
-
-
-
+print_head() {
+echo -e "\e[35m>>>>>>>> $1 <<<<<<<<<\e[0m"
+}
 func_nodejs() {
-    
-echo -e "\e[36m>>>>>>>> configuring nodejs repos <<<<<<<<<\e[0m"
+
+
+ print_head "configuring nodejs repos" 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
 
-echo -e "\e[36m>>>>>>>> install nodejs <<<<<<<<<\e[0m"
+ print_head "install nodejs" 
 yum install nodejs -y
 
 
-echo -e "\e[36m>>>>>>>> add application user <<<<<<<<<\e[0m"
+
+ print_head "add application user" 
 useradd ${app_user}
 
-echo -e "\e[36m>>>>>>>> create application directory <<<<<<<<<\e[0m"
+
+ print_head "create application directory" 
 rm -rf /app
 mkdir /app 
 
-echo -e "\e[36m>>>>>>>> download app content <<<<<<<<<\e[0m"
+
+ print_head "download app content" 
 curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip 
 cd /app 
 
-echo -e "\e[36m>>>>>>>> unzip app content <<<<<<<<<\e[0m"
+
+print_head "unzip app content" 
 unzip /tmp/${component}.zip
 
-echo -e "\e[36m>>>>>>>> install nodejs dependencies <<<<<<<<<\e[0m"
+
+ print_head "install nodejs dependencies" 
 npm install 
 
-echo -e "\e[36m>>>>>>>> copy user systemd file <<<<<<<<<\e[0m"
+
+print_head "copy user systemd file" 
 cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
 
-echo -e "\e[36m>>>>>>>> start cart service <<<<<<<<<\e[0m"
+
+ print_head "start cart service" 
 systemctl daemon-reload
 systemctl enable ${component}
 systemctl start ${component}
