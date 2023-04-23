c9933586c58a2d81646e245da1e0b6b0b805351d
sukanya <sukanyasirigiri@gmail.com>
2023-04-23 11:25:55 -0400

    script cart

--- cart.sh
+++ cart.sh
@@ -1,4 +1,7 @@
-source common.sh
+script=$(realpath "0")
+script_path=$(dirname "$script")
+source ${script_path}/common.sh
+
 echo -e "\e[36m>>>>>>>> configuring nodejs repos <<<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
