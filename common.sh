25d83832e3e93a8c8e3ee9a53bcf156569800665
sukanya <sukanyasirigiri@gmail.com>
2023-04-30 14:31:29 -0400

    common

--- common.sh
+++ common.sh
@@ -89,6 +89,12 @@ func_java() {
 print_head "install maven"
 yum install maven -y
 
+if [ $? -eq 0 ]; then
+echo -e "\e[32mSUCCESS\e[0m"
+else
+echo -e "\e[31mFAILURE\e[0m"
+fi
+
 
 func_app_prereq
 
