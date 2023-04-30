419e6712f7c26527a721f5e4b2550e9b0101f1e8
sukanya <sukanyasirigiri@gmail.com>
2023-04-30 16:09:36 -0400

    frontend

--- frontend.sh
+++ frontend.sh
@@ -4,30 +4,30 @@ source ${script_path}/common.sh
 
 
 
-prin_head "install Nginx"
+print_head "install Nginx"
 yum install nginx -y &>>$log_file
 func_stat_check $?
 
 
-prin_head "copy roboshop file"
+print_head "copy roboshop file"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf &>>$log_file
 func_stat_check $?
 
 
-prin_head "Clen old app content"
+print_head "Clen old app content"
 rm -rf /usr/share/nginx/html/* &>>$log_file
 func_stat_check $?
 
-prin_head "Download app ontent"
+print_head "Download app ontent"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>$log_file
 func_stat_check $?
 
-prin_head "Extract app content"
+print_head "Extract app content"
 cd /usr/share/nginx/html &>>$log_file
 unzip /tmp/frontend.zip &>>$log_file
 func_stat_check $?
 
-prin_head "start Nginx"
+print_head "start Nginx"
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
 func_stat_check $?
